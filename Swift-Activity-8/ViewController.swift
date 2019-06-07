bb4b80b3dc0a1f0f0626c4352e193d1ac9e179dc
DeAndre <de.andre.badresingh@brooklynsteamcenter.org>
2019-06-07 14:18:12 -0400

    Initial Commit

---
+++ Swift-Activity-8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Activity-8
+//
+//  Created by Genesis on 6/7/19.
+//  Copyright © 2019 DeAndre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Swift-Activity-8Tests/Swift_Activity_8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_Activity_8Tests.swift
+//  Swift-Activity-8Tests
+//
+//  Created by Genesis on 6/7/19.
+//  Copyright © 2019 DeAndre. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Activity_8
+
+class Swift_Activity_8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
